8fd610f75f4bda1b5a99839b88fdfa18e240455e
thirotan <thirotanjp@gmail.com>
2016-09-01 13:57:30 +0900

    fix config.ru & test_helper

--- config.ru
+++ config.ru
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-#LOAD_PATH.unshift(File.dirname(__FILE__) + '/lib/')
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/lib/')
 
 require 'rack/urlmap'
 require 'sinatraapp/app'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,12 @@
 require 'simplecov'
+require sequel
+
+config ||= YAML.load_file(settings.root+"config.yaml")
+db_config = ENV['RACK_ENV']
+dsn = config[db_config] 
+
+DB = Sequel.connect(dsn)
+
 SimpleCov.start do
   add_filter '/test/'
 end
